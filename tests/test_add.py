262ea527af47ddbcb9af9317629700760bd99aad
Linar Khamidulin <hamidulin16@yandex.ru>
2024-12-22 16:31:35 +0300

    commit add file 22.12

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.classes import Product, Category
+from src.classes import Product, Category, Smartphone, LawnGrass
 
 
 @pytest.fixture
@@ -18,3 +18,20 @@ def product_2():
 def test_add_product(product_1, product_2):
 
     assert product_1 + product_2 == 23
+
+
+@pytest.fixture
+def product_3():
+    product_3 = Smartphone('prod_1', "1", 2, 10, 2.3, "model",  1, "color")
+    return product_3
+
+
+@pytest.fixture
+def product_4():
+    product_4 = LawnGrass('prod_2', "1", 3, 1, "country", "germination_period", "color")
+    return product_4
+
+
+def test_add_product_error(product_3, product_4):
+    with pytest.raises(TypeError):
+        product_3 + product_4
